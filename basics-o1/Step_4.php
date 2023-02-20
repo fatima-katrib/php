5823408bfb351336db0b8dac2e3f135ea82f5984
fatima-katrib <fatimakatrib19@gnail.com>
2023-02-20 08:21:51 +0200

    step-4

---
+++ basics-o1/Step_4.php
@@ -0,0 +1,10 @@
+<?php
+$name="fatima";
+$position="intern";
+$github_url="https://github.com/fatima";
+
+
+echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$github_url."<br>";
+echo "Hello, I'm ",$name,", I'm a ",$position," please check my github link ",$github_url,"<br>";
+echo "Hello, I'm ${name}, I'm a ${position} please check my github link ${github_url}.";
+?>
